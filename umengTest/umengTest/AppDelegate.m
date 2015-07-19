a5c85ca72e4a00eb0ee79ae7f3968d266e48073a
hehe <hehe@163.com>
2015-07-19 19:17:53 +0800

    添加系统回调

--- umengTest/umengTest/AppDelegate.m
+++ umengTest/umengTest/AppDelegate.m
@@ -29,6 +29,20 @@
     return YES;
 }
 
+#warning 添加系统回调
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url
+{
+    return  [UMSocialSnsService handleOpenURL:url];
+}
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    return  [UMSocialSnsService handleOpenURL:url];
+}
+
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
